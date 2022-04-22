c412a15292f8ddec79559e54fd9e55e0a300873e
Dean Thompson <deanthompson.music@gmail.com>
2022-04-22 13:43:37 +0900

    Added logout protocol/delegate and finished creating a dummyVC

--- BankyBanking/AppDelegate.swift
+++ BankyBanking/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
+    let dummyViewController = DummyViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
@@ -23,8 +24,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         loginViewController.delegate = self
         onboardingContainerViewController.delegate = self
+        dummyViewController.logoutDelegate = self
         
-        window?.rootViewController = DummyViewController()
+        window?.rootViewController = loginViewController
         
         
         
@@ -42,7 +44,13 @@ extension AppDelegate: LoginViewControllerDelegate {
 
 extension AppDelegate: OnboardingContainerViewControllerDelegate {
     func didFinishOnboarding() {
-        print("DEBUG: finished on boarding")
+        setRootViewController(dummyViewController)
+    }
+}
+
+extension AppDelegate: LogoutDelegate {
+    func didLogout() {
+        setRootViewController(loginViewController)
     }
 }
 
--- BankyBanking/DummyViewController.swift
+++ BankyBanking/DummyViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class DummyViewController: UIViewController {
     
+    let stackView = UIStackView()
     let label = UILabel()
+    let logoutButton = UIButton()
+    
+    weak var logoutDelegate: LogoutDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemCyan
+        view.backgroundColor = .systemBackground
         
         style()
         layout()
@@ -24,18 +28,37 @@ class DummyViewController: UIViewController {
 
 extension DummyViewController {
     private func style() {
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "DUMMY VIEW"
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        label.textAlignment = .center
+        
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.configuration = .filled()
+        logoutButton.setTitle("Log Out", for: [])
+        logoutButton.addTarget(self, action: #selector(logoutButtonTapped), for: .primaryActionTriggered)
     }
     
     private func layout() {
-        
-        view.addSubview(label)
+        stackView.addArrangedSubview(label)
+        stackView.addArrangedSubview(logoutButton)
+        view.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
             
         ])
+    }
+    
+    @objc func logoutButtonTapped(sender: UIButton) {
+        logoutDelegate?.didLogout()
     }
 }
