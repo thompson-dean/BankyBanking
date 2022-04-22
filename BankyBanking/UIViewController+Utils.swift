cd881ad3848da7283d8d1e956dc47e153675e58b
Dean Thompson <deanthompson.music@gmail.com>
2022-04-22 14:51:58 +0900

    Added TabBar

---
+++ BankyBanking/UIViewController+Utils.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController+Utils.swift
+//  BankyBanking
+//
+//  Created by Dean Thompson on 2022/04/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let statusBarSize = UIApplication.shared.statusBarFrame.size
+        let frame = CGRect(origin: .zero, size: statusBarSize)
+        let statusbarView = UIView(frame: frame)
+        
+        statusbarView.backgroundColor = appColor
+        view.addSubview(statusbarView)
+    }
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
