244d534d50a52efda15f9efee1dc0b09431090bc
Dean Thompson <deanthompson.music@gmail.com>
2022-04-25 16:54:13 +0900

    Added unit tests for JSON Parsing

---
+++ BankyBankingUnitTests/AccountSummary/ProfileTests.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileTests.swift
+//  BankyBankingUnitTests
+//
+//  Created by Dean Thompson on 2022/04/25.
+//
+
+import Foundation
+import XCTest
+
+@testable import BankyBanking
+
+class ProfileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testCanParse() throws {
+        let json = """
+        {
+        "id": "1",
+        "first_name": "Kevin",
+        "last_name": "Flynn",
+        }
+        """
+        
+        let data = json.data(using: .utf8)!
+        let result = try! JSONDecoder().decode(Profile.self, from: data)
+        
+        XCTAssertEqual(result.id, "1")
+        XCTAssertEqual(result.firstName, "Kevin")
+        XCTAssertEqual(result.lastName, "Flynn")
+    }
+}
